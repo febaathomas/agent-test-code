6ede13599143b14487a56d9971021ee9232542d5
febaathomas <131870140+febaathomas@users.noreply.github.com>
2026-04-27 14:31:16 +0530

    feat: KAN-31 terraform

--- generated/KAN-31/main.tf
+++ generated/KAN-31/main.tf
@@ -1,5 +1,4 @@
 terraform {
-  required_version = ">= 1.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -9,30 +8,65 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = var.aws_region
 }
 
-variable "environment" {
-  description = "Environment name"
+variable "aws_region" {
+  description = "AWS region"
   type        = string
-  default     = "dev"
+  default     = "us-east-1"
+}
+
+resource "aws_security_group" "ssh" {
+  name        = "kan-31-ssh-sg"
+  description = "Allow SSH inbound"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "KAN-31-ssh-sg"
+  }
 }
 
 data "aws_ami" "amazon_linux" {
   most_recent = true
   owners      = ["amazon"]
-
   filter {
     name   = "name"
     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
 }
 
-resource "aws_instance" "simple_ec2" {
-  ami           = data.aws_ami.amazon_linux.id
-  instance_type = "t2.micro"
+resource "aws_instance" "example" {
+  ami                         = data.aws_ami.amazon_linux.id
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  subnet_id                   = element(data.aws_subnet_ids.default.ids, 0)
+  vpc_security_group_ids      = [aws_security_group.ssh.id]
 
   tags = {
-    Name = "simple-ec2-${var.environment}"
+    Name = "KAN-31-ec2"
   }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
 }
