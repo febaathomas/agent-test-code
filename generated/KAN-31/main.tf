d4a9568552787db13d66e08135e4c16f34358986
febaathomas <131870140+febaathomas@users.noreply.github.com>
2026-05-11 14:04:54 +0530

    feat: KAN-31 terraform

--- generated/KAN-31/main.tf
+++ generated/KAN-31/main.tf
@@ -9,42 +9,30 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = var.aws_region
+}
+
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+resource "aws_instance" "kan_31_ec2" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "KAN-31-ec2"
+  }
 }
 
 data "aws_ami" "amazon_linux" {
   most_recent = true
   owners      = ["amazon"]
+
   filter {
     name   = "name"
     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-}
-
-resource "aws_security_group" "ssh" {
-  name        = "ssh-allow"
-  description = "Allow SSH inbound traffic"
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_instance" "example" {
-  ami           = data.aws_ami.amazon_linux.id
-  instance_type = "t2.micro"
-  security_groups = [aws_security_group.ssh.name]
-
-  tags = {
-    Name = "KAN-31-ec2"
   }
 }
