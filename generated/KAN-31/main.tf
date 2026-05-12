2d571661ada8e4fc4a84e1891e0605fc33626429
febaathomas <131870140+febaathomas@users.noreply.github.com>
2026-05-12 13:06:02 +0530

    feat: KAN-31 terraform

--- generated/KAN-31/main.tf
+++ generated/KAN-31/main.tf
@@ -8,13 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  region = var.aws_region
-}
-
-variable "aws_region" {
-  description = "AWS region to deploy resources"
-  type        = string
-  default     = "us-east-1"
+  region = "us-east-1"
 }
 
 data "aws_ami" "amazon_linux" {
@@ -27,11 +21,11 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
-resource "aws_instance" "simple" {
+resource "aws_instance" "example" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
 
   tags = {
-    Name = "simple-ec2"
+    Name = "KAN-31-ec2"
   }
 }
