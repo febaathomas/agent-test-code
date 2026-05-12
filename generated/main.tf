e0f7489fb7a41e93e4a651978f91ce26a6ea7a13
febaathomas <131870140+febaathomas@users.noreply.github.com>
2026-05-12 18:02:34 +0530

    Add Terraform config for KAN-31: create an ec2 instance on aws with minimum basic requirements

---
+++ generated/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t"  # placeholder
+
+  tags = {
+    Name = "example-ec2"
+  }
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
