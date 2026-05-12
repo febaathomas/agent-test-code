4c450b5b9b912db8e911f78c78b2adc28f93fe78
febaathomas <131870140+febaathomas@users.noreply.github.com>
2026-05-12 12:14:26 +0530

    feat: KAN-31 terraform

--- generated/KAN-31/main.tf
+++ generated/KAN-31/main.tf
@@ -1,4 +1,8 @@
+# Summary: create an ec2 instance on aws with minimum basic requirements
+# Description: Just need a simple ec2 instance running on amazon linux
+
 terraform {
+  required_version = ">= 1.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -12,61 +16,26 @@ provider "aws" {
 }
 
 variable "aws_region" {
-  description = "AWS region"
+  description = "AWS region to deploy resources"
   type        = string
   default     = "us-east-1"
 }
 
-resource "aws_security_group" "ssh" {
-  name        = "kan-31-ssh-sg"
-  description = "Allow SSH inbound"
-  vpc_id      = data.aws_vpc.default.id
-
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
 
   tags = {
-    Name = "KAN-31-ssh-sg"
+    Name = "KAN-31-EC2"
   }
 }
 
 data "aws_ami" "amazon_linux" {
   most_recent = true
   owners      = ["amazon"]
+
   filter {
     name   = "name"
     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
-}
-
-resource "aws_instance" "example" {
-  ami                         = data.aws_ami.amazon_linux.id
-  instance_type               = "t2.micro"
-  associate_public_ip_address = true
-  subnet_id                   = element(data.aws_subnet_ids.default.ids, 0)
-  vpc_security_group_ids      = [aws_security_group.ssh.id]
-
-  tags = {
-    Name = "KAN-31-ec2"
-  }
-}
-
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.default.id
 }
